f3c00955f8f2753859d9d27375ec58953755df36
Sorias05 <sashko2b3@gmail.com>
2024-03-07 13:12:52 +0200

    init

---
+++ script.js
@@ -0,0 +1,212 @@
+var player = document.getElementById("player");
+var seekSlider = document.getElementById("seek-slider");
+var currentTime = document.getElementById("current-time");
+var duration = document.getElementById("duration");
+var volumeSlider = document.getElementById("volume-slider");
+player.isPlaying = false;
+player.volume = volumeSlider.value / 100;
+var r = document.querySelector(':root');
+var playerVolume = player.volume;
+var shuffle = false;
+var replay = 1;
+var mute = false;
+var showPlayingTab = false;
+var showText = false;
+var showQueue = false;
+var showDevice = false;
+var showMiniplayer = false;
+var fullscreen = false;
+
+document.querySelectorAll('[id="btn-play"]').forEach(btn => btn.addEventListener('click', (e) => {
+    player.isPlaying = !player.isPlaying;
+    if (player.isPlaying) {
+        document.querySelectorAll('[id="btn-play"]').forEach(btn => btn.innerHTML = '<path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>');
+        r.style.setProperty("--display", "block");
+        player.play();
+    }
+    else {
+        document.querySelectorAll('[id="btn-play"]').forEach(btn => btn.innerHTML = '<path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>');
+        r.style.setProperty("--display", "none");
+        player.pause();
+    }
+}));
+
+function btnShuffleClick() {
+    shuffle = !shuffle;
+    if (shuffle){
+        document.getElementById("btn-shuffle").innerHTML = '<path fill="#1ed760" d="M13.151.922a.75.75 0 1 0-1.06 1.06L13.109 3H11.16a3.75 3.75 0 0 0-2.873 1.34l-6.173 7.356A2.25 2.25 0 0 1 .39 12.5H0V14h.391a3.75 3.75 0 0 0 2.873-1.34l6.173-7.356a2.25 2.25 0 0 1 1.724-.804h1.947l-1.017 1.018a.75.75 0 0 0 1.06 1.06L15.98 3.75 13.15.922zM.391 3.5H0V2h.391c1.109 0 2.16.49 2.873 1.34L4.89 5.277l-.979 1.167-1.796-2.14A2.25 2.25 0 0 0 .39 3.5z"></path><path fill="#1ed760" d="m7.5 10.723.98-1.167.957 1.14a2.25 2.25 0 0 0 1.724.804h1.947l-1.017-1.018a.75.75 0 1 1 1.06-1.06l2.829 2.828-2.829 2.828a.75.75 0 1 1-1.06-1.06L13.109 13H11.16a3.75 3.75 0 0 1-2.873-1.34l-.787-.938z"></path>';
+        document.getElementById("circle-shuffle").classList.add('circle-visible');
+    }
+    else{
+        document.getElementById("btn-shuffle").innerHTML = '<path fill="#fff" d="M13.151.922a.75.75 0 1 0-1.06 1.06L13.109 3H11.16a3.75 3.75 0 0 0-2.873 1.34l-6.173 7.356A2.25 2.25 0 0 1 .39 12.5H0V14h.391a3.75 3.75 0 0 0 2.873-1.34l6.173-7.356a2.25 2.25 0 0 1 1.724-.804h1.947l-1.017 1.018a.75.75 0 0 0 1.06 1.06L15.98 3.75 13.15.922zM.391 3.5H0V2h.391c1.109 0 2.16.49 2.873 1.34L4.89 5.277l-.979 1.167-1.796-2.14A2.25 2.25 0 0 0 .39 3.5z"></path><path fill="#fff" d="m7.5 10.723.98-1.167.957 1.14a2.25 2.25 0 0 0 1.724.804h1.947l-1.017-1.018a.75.75 0 1 1 1.06-1.06l2.829 2.828-2.829 2.828a.75.75 0 1 1-1.06-1.06L13.109 13H11.16a3.75 3.75 0 0 1-2.873-1.34l-.787-.938z"></path>';
+        document.getElementById("circle-shuffle").classList.remove('circle-visible');
+    }
+}
+
+function btnReplayClick() {
+    if (replay >= 3)
+        replay = 1;
+    else
+        replay += 1;
+    switch (replay) {
+        case 1: document.getElementById("btn-replay").innerHTML = '<path fill="#fff" d="M0 4.75A3.75 3.75 0 0 1 3.75 1h8.5A3.75 3.75 0 0 1 16 4.75v5a3.75 3.75 0 0 1-3.75 3.75H9.81l1.018 1.018a.75.75 0 1 1-1.06 1.06L6.939 12.75l2.829-2.828a.75.75 0 1 1 1.06 1.06L9.811 12h2.439a2.25 2.25 0 0 0 2.25-2.25v-5a2.25 2.25 0 0 0-2.25-2.25h-8.5A2.25 2.25 0 0 0 1.5 4.75v5A2.25 2.25 0 0 0 3.75 12H5v1.5H3.75A3.75 3.75 0 0 1 0 9.75v-5z"></path>'; 
+                document.getElementById("circle-replay").classList.remove('circle-visible');
+                break;
+        case 2: document.getElementById("btn-replay").innerHTML = '<path fill="#1ed760" d="M0 4.75A3.75 3.75 0 0 1 3.75 1h8.5A3.75 3.75 0 0 1 16 4.75v5a3.75 3.75 0 0 1-3.75 3.75H9.81l1.018 1.018a.75.75 0 1 1-1.06 1.06L6.939 12.75l2.829-2.828a.75.75 0 1 1 1.06 1.06L9.811 12h2.439a2.25 2.25 0 0 0 2.25-2.25v-5a2.25 2.25 0 0 0-2.25-2.25h-8.5A2.25 2.25 0 0 0 1.5 4.75v5A2.25 2.25 0 0 0 3.75 12H5v1.5H3.75A3.75 3.75 0 0 1 0 9.75v-5z"></path>'; 
+                document.getElementById("circle-replay").classList.add('circle-visible');
+                break;
+        case 3: document.getElementById("btn-replay").innerHTML = '<path fill="#1ed760" d="M0 4.75A3.75 3.75 0 0 1 3.75 1h.75v1.5h-.75A2.25 2.25 0 0 0 1.5 4.75v5A2.25 2.25 0 0 0 3.75 12H5v1.5H3.75A3.75 3.75 0 0 1 0 9.75v-5zM12.25 2.5h-.75V1h.75A3.75 3.75 0 0 1 16 4.75v5a3.75 3.75 0 0 1-3.75 3.75H9.81l1.018 1.018a.75.75 0 1 1-1.06 1.06L6.939 12.75l2.829-2.828a.75.75 0 1 1 1.06 1.06L9.811 12h2.439a2.25 2.25 0 0 0 2.25-2.25v-5a2.25 2.25 0 0 0-2.25-2.25z"></path><path fill="#1ed760" d="M9.12 8V1H7.787c-.128.72-.76 1.293-1.787 1.313V3.36h1.57V8h1.55z"></path>'; break;
+    }
+}
+
+function btnPrevClick() {
+    player.currentTime = 0;
+}
+
+function btnMuteClick() {
+    mute = !mute;
+    if (mute) {
+        playerVolume = player.volume;
+        player.volume = 0;
+        setMuteIcon();
+        r.style.setProperty("--volume", "0");
+        volumeSlider.value = 0;
+    }
+    else {
+        player.volume = playerVolume;
+        setMuteIcon();
+        r.style.setProperty("--volume", (player.volume * 100) + "%");
+        volumeSlider.value = playerVolume * 100;
+    }
+}
+
+function btnPlayingTabClick() {
+    showPlayingTab = !showPlayingTab;
+    if (showPlayingTab)
+    {
+        document.getElementById("btn-playing-tab").innerHTML = '<path fill="#1ed760" d="M11.196 8 6 5v6l5.196-3z"></path><path fill="#1ed760" d="M15.002 1.75A1.75 1.75 0 0 0 13.252 0h-10.5a1.75 1.75 0 0 0-1.75 1.75v12.5c0 .966.783 1.75 1.75 1.75h10.5a1.75 1.75 0 0 0 1.75-1.75V1.75zm-1.75-.25a.25.25 0 0 1 .25.25v12.5a.25.25 0 0 1-.25.25h-10.5a.25.25 0 0 1-.25-.25V1.75a.25.25 0 0 1 .25-.25h10.5z"></path>';
+        document.getElementById("btn-now-playing-icon").innerHTML = '<path fill="#fff" d="M.47 4.97a.75.75 0 0 1 1.06 0L8 11.44l6.47-6.47a.75.75 0 1 1 1.06 1.06L8 13.56.47 6.03a.75.75 0 0 1 0-1.06z"></path>'
+        document.getElementById("circle-playing-tab").classList.add('circle-visible');
+        document.getElementById("full-body-div").style.gridTemplateAreas = "'left main right''footer footer footer'";
+        document.getElementById("right-side").style.display="grid";
+    }
+    else
+    {
+        document.getElementById("btn-playing-tab").innerHTML = '<path fill="#fff" d="M11.196 8 6 5v6l5.196-3z"></path><path fill="#fff" d="M15.002 1.75A1.75 1.75 0 0 0 13.252 0h-10.5a1.75 1.75 0 0 0-1.75 1.75v12.5c0 .966.783 1.75 1.75 1.75h10.5a1.75 1.75 0 0 0 1.75-1.75V1.75zm-1.75-.25a.25.25 0 0 1 .25.25v12.5a.25.25 0 0 1-.25.25h-10.5a.25.25 0 0 1-.25-.25V1.75a.25.25 0 0 1 .25-.25h10.5z"></path>';
+        document.getElementById("btn-now-playing-icon").innerHTML = '<path fill="#fff" d="M.47 11.03a.75.75 0 0 0 1.06 0L8 4.56l6.47 6.47a.75.75 0 1 0 1.06-1.06L8 2.44.47 9.97a.75.75 0 0 0 0 1.06z"></path>'
+        document.getElementById("circle-playing-tab").classList.remove('circle-visible');
+        document.getElementById("full-body-div").style.gridTemplateAreas = "'left main''footer footer'";
+        document.getElementById("right-side").style.display="none";
+    }
+}
+
+function btnTextClick() {
+    showText = !showText;
+    if (showText)
+    {
+        document.getElementById("btn-text").innerHTML = '<path fill="#1ed760" d="M13.426 2.574a2.831 2.831 0 0 0-4.797 1.55l3.247 3.247a2.831 2.831 0 0 0 1.55-4.797zM10.5 8.118l-2.619-2.62A63303.13 63303.13 0 0 0 4.74 9.075L2.065 12.12a1.287 1.287 0 0 0 1.816 1.816l3.06-2.688 3.56-3.129zM7.12 4.094a4.331 4.331 0 1 1 4.786 4.786l-3.974 3.493-3.06 2.689a2.787 2.787 0 0 1-3.933-3.933l2.676-3.045 3.505-3.99z"></path>';
+        document.getElementById("circle-text").classList.add('circle-visible');
+    }
+    else
+    {
+        document.getElementById("btn-text").innerHTML = '<path fill="#fff" d="M13.426 2.574a2.831 2.831 0 0 0-4.797 1.55l3.247 3.247a2.831 2.831 0 0 0 1.55-4.797zM10.5 8.118l-2.619-2.62A63303.13 63303.13 0 0 0 4.74 9.075L2.065 12.12a1.287 1.287 0 0 0 1.816 1.816l3.06-2.688 3.56-3.129zM7.12 4.094a4.331 4.331 0 1 1 4.786 4.786l-3.974 3.493-3.06 2.689a2.787 2.787 0 0 1-3.933-3.933l2.676-3.045 3.505-3.99z"></path>';
+        document.getElementById("circle-text").classList.remove('circle-visible');
+    }
+}
+
+function btnQueueClick() {
+    showQueue = !showQueue;
+    if (showQueue)
+    {
+        document.getElementById("btn-queue").innerHTML = '<path fill="#1ed760" d="M15 15H1v-1.5h14V15zm0-4.5H1V9h14v1.5zm-14-7A2.5 2.5 0 0 1 3.5 1h9a2.5 2.5 0 0 1 0 5h-9A2.5 2.5 0 0 1 1 3.5zm2.5-1a1 1 0 0 0 0 2h9a1 1 0 1 0 0-2h-9z"></path>';
+        document.getElementById("circle-queue").classList.add('circle-visible');
+    }
+    else
+    {
+        document.getElementById("btn-queue").innerHTML = '<path fill="#fff" d="M15 15H1v-1.5h14V15zm0-4.5H1V9h14v1.5zm-14-7A2.5 2.5 0 0 1 3.5 1h9a2.5 2.5 0 0 1 0 5h-9A2.5 2.5 0 0 1 1 3.5zm2.5-1a1 1 0 0 0 0 2h9a1 1 0 1 0 0-2h-9z"></path>';
+        document.getElementById("circle-queue").classList.remove('circle-visible');
+    }
+}
+
+function aQueueClick() {
+    showQueue = true;
+    document.getElementById("btn-queue").innerHTML = '<path fill="#1ed760" d="M15 15H1v-1.5h14V15zm0-4.5H1V9h14v1.5zm-14-7A2.5 2.5 0 0 1 3.5 1h9a2.5 2.5 0 0 1 0 5h-9A2.5 2.5 0 0 1 1 3.5zm2.5-1a1 1 0 0 0 0 2h9a1 1 0 1 0 0-2h-9z"></path>';
+    document.getElementById("circle-queue").classList.add('circle-visible');
+}
+
+function btnDeviceClick() {
+    // showDevice = !showDevice;
+    // if (showDevice)
+    //     document.getElementById("btn-device").innerHTML = '<path fill="#1ed760" d="M11.196 8 6 5v6l5.196-3z"></path><path fill="#1ed760" d="M15.002 1.75A1.75 1.75 0 0 0 13.252 0h-10.5a1.75 1.75 0 0 0-1.75 1.75v12.5c0 .966.783 1.75 1.75 1.75h10.5a1.75 1.75 0 0 0 1.75-1.75V1.75zm-1.75-.25a.25.25 0 0 1 .25.25v12.5a.25.25 0 0 1-.25.25h-10.5a.25.25 0 0 1-.25-.25V1.75a.25.25 0 0 1 .25-.25h10.5z"></path>';
+    // else
+    //     document.getElementById("btn-device").innerHTML = '<path fill="#fff" d="M11.196 8 6 5v6l5.196-3z"></path><path fill="#fff" d="M15.002 1.75A1.75 1.75 0 0 0 13.252 0h-10.5a1.75 1.75 0 0 0-1.75 1.75v12.5c0 .966.783 1.75 1.75 1.75h10.5a1.75 1.75 0 0 0 1.75-1.75V1.75zm-1.75-.25a.25.25 0 0 1 .25.25v12.5a.25.25 0 0 1-.25.25h-10.5a.25.25 0 0 1-.25-.25V1.75a.25.25 0 0 1 .25-.25h10.5z"></path>';
+}
+
+function btnMiniplayerClick() {
+    showMiniplayer = !showMiniplayer;
+    if (showMiniplayer)
+    {
+        document.getElementById("btn-miniplayer").innerHTML = '<path fill="#1ed760" d="M16 2.45c0-.8-.65-1.45-1.45-1.45H1.45C.65 1 0 1.65 0 2.45v11.1C0 14.35.65 15 1.45 15h5.557v-1.5H1.5v-11h13V7H16V2.45z"></path><path fill="#1ed760" d="M15.25 9.007a.75.75 0 0 1 .75.75v4.493a.75.75 0 0 1-.75.75H9.325a.75.75 0 0 1-.75-.75V9.757a.75.75 0 0 1 .75-.75h5.925z"></path>';
+        document.getElementById("circle-miniplayer").classList.add('circle-visible');
+    }
+    else
+    {
+        document.getElementById("btn-miniplayer").innerHTML = '<path fill="#fff" d="M16 2.45c0-.8-.65-1.45-1.45-1.45H1.45C.65 1 0 1.65 0 2.45v11.1C0 14.35.65 15 1.45 15h5.557v-1.5H1.5v-11h13V7H16V2.45z"></path><path fill="#fff" d="M15.25 9.007a.75.75 0 0 1 .75.75v4.493a.75.75 0 0 1-.75.75H9.325a.75.75 0 0 1-.75-.75V9.757a.75.75 0 0 1 .75-.75h5.925z"></path>';
+        document.getElementById("circle-miniplayer").classList.remove('circle-visible');
+    }
+}
+
+function btnFullscreenClick() {
+    // fullscreen = !fullscreen;
+    // if (fullscreen)
+    //     document.getElementById("btn-fullscreen").innerHTML = '<path fill="#1ed760" d="M11.196 8 6 5v6l5.196-3z"></path><path fill="#1ed760" d="M15.002 1.75A1.75 1.75 0 0 0 13.252 0h-10.5a1.75 1.75 0 0 0-1.75 1.75v12.5c0 .966.783 1.75 1.75 1.75h10.5a1.75 1.75 0 0 0 1.75-1.75V1.75zm-1.75-.25a.25.25 0 0 1 .25.25v12.5a.25.25 0 0 1-.25.25h-10.5a.25.25 0 0 1-.25-.25V1.75a.25.25 0 0 1 .25-.25h10.5z"></path>';
+    // else
+    //     document.getElementById("btn-fullscreen").innerHTML = '<path fill="#fff" d="M11.196 8 6 5v6l5.196-3z"></path><path fill="#fff" d="M15.002 1.75A1.75 1.75 0 0 0 13.252 0h-10.5a1.75 1.75 0 0 0-1.75 1.75v12.5c0 .966.783 1.75 1.75 1.75h10.5a1.75 1.75 0 0 0 1.75-1.75V1.75zm-1.75-.25a.25.25 0 0 1 .25.25v12.5a.25.25 0 0 1-.25.25h-10.5a.25.25 0 0 1-.25-.25V1.75a.25.25 0 0 1 .25-.25h10.5z"></path>';
+}
+
+function setMuteIcon() {
+    if (player.volume <= 0.25 && player.volume > 0)
+        document.getElementById("btn-mute").innerHTML = '<path fill="#fff" d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z"></path>'
+    else if (player.volume >= 0.7)
+        document.getElementById("btn-mute").innerHTML = '<path fill="#fff" d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z"></path><path fill="#fff" d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127v1.55z"></path>'
+    else if (player.volume <= 0)
+        document.getElementById("btn-mute").innerHTML = '<path fill="#fff" d="M13.86 5.47a.75.75 0 0 0-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 0 0 8.8 6.53L10.269 8l-1.47 1.47a.75.75 0 1 0 1.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 0 0 1.06-1.06L12.39 8l1.47-1.47a.75.75 0 0 0 0-1.06z"></path><path fill="#fff" d="M10.116 1.5A.75.75 0 0 0 8.991.85l-6.925 4a3.642 3.642 0 0 0-1.33 4.967 3.639 3.639 0 0 0 1.33 1.332l6.925 4a.75.75 0 0 0 1.125-.649v-1.906a4.73 4.73 0 0 1-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 0 1-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path>'
+    else
+        document.getElementById("btn-mute").innerHTML = '<path fill="#fff" d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 6.087a4.502 4.502 0 0 0 0-8.474v1.65a2.999 2.999 0 0 1 0 5.175v1.649z"></path>'
+}
+
+const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${minutes}:${returnedSeconds}`;
+}
+
+const setSliderMax = () => {
+    seekSlider.max = Math.floor(audio.duration);
+}
+
+function playerUpdate() {
+    seekSlider.value = player.currentTime / player.duration * 100000;
+    r.style.setProperty("--progress-bar-transform", seekSlider.value / 1000 + "%")
+    currentTime.innerHTML = calculateTime(player.currentTime);
+    duration.innerHTML = calculateTime(player.duration);
+}
+
+document.getElementById("next-in-queue").addEventListener('mouseover', (e) => {
+    document.getElementById("next-in-queue-btn").innerHTML = '<path fill="#fff" d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>'
+});
+
+document.getElementById("next-in-queue").addEventListener('mouseout', (e) => {
+    document.getElementById("next-in-queue-btn").innerHTML = '<path fill="#fff" d="M10 2v9.5a2.75 2.75 0 1 1-2.75-2.75H8.5V2H10zm-1.5 8.25H7.25A1.25 1.25 0 1 0 8.5 11.5v-1.25z"></path>'
+});
+
+seekSlider.addEventListener('input', (e) => {
+    player.currentTime = e.target.value / 100000 * player.duration;
+});
+
+volumeSlider.addEventListener('input', (e) => {
+    player.volume = e.target.value / 100;
+    r.style.setProperty("--volume", e.target.value + "%")
+    setMuteIcon();
+});
